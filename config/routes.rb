67e841e31431cfaf03a05cda3aa8f1a8c249b783
bria_white <bria.awhite@gmail.com>
2021-06-03 01:32:02 -0400

    api controller & routes namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :beaches
+  # resources :comments
+  namespace :api do
+    namespace :v1 do 
+      resources :beaches, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
