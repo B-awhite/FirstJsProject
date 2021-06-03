3b6b2615e2697057becfff46a3180c51568260ce
bria_white <bria.awhite@gmail.com>
2021-06-03 01:23:47 -0400

    Rails scaffold, migrations, & model associations

---
+++ db/migrate/20210603051816_create_beaches.rb
@@ -0,0 +1,10 @@
+class CreateBeaches < ActiveRecord::Migration[6.1]
+  def change
+    create_table :beaches do |t|
+      t.string :name
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
