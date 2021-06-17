d9957cfb3652b2f02c53eccebd4002a040698ac0
bria_white <bria.awhite@gmail.com>
2021-06-17 19:23:13 -0400

    comments validations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # resources :comments
   namespace :api do
     namespace :v1 do 
-      resources :beaches
+      resources :beaches do
+        resources :comments, only: [:new, :create, :index]
+      end 
     end
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
